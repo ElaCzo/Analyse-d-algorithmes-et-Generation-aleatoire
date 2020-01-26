f65c966e8b70588b899c60649c25207598d6e6cf
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2020-01-27 00:49:08 +0100

    Correction du test de couverture de Remy WIP

--- src/main/java/Remy/Remy.java
+++ src/main/java/Remy/Remy.java
@@ -1,24 +1,32 @@
 package main.java.Remy;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Remy {
     public long N;
     public Node[] tree;
 
+
+    public Remy(long n, int[] list, int a) {
+
+    }
+
     // construction of a growing binary tree with n internal nodes (n <= N)
     // modifies the position of two leaves, does not modify the tree built
-    public void change_leaves(int a, int b){
+    public void change_leaves(int a, int b) {
         int parenta, parentb;
         parenta = tree[a].parent;
         parentb = tree[b].parent;
 
-        if(tree[parenta].right_child == a)
+        if (tree[parenta].right_child == a)
             tree[parenta].right_child = b;
         else
             tree[parenta].left_child = b;
 
         tree[a].parent = parentb;
 
-        if(tree[parentb].right_child == b)
+        if (tree[parentb].right_child == b)
             tree[parentb].right_child = a;
         else
             tree[parentb].left_child = a;
@@ -28,8 +36,8 @@ public class Remy {
 
     public Remy(long n) {
         long i, number;
-        N=n;
-        tree = new Node[2*(int)N +1];
+        N = n;
+        tree = new Node[2 * (int) N + 1];
 
         // built a tree of size 1
         tree[0].left_child = 1;
@@ -42,26 +50,26 @@ public class Remy {
         // the internal nodes will be in the boxes [0; i-1[ of the tree's array
         // the leaves in boxes [i, 2*i[ of the tree's array
 
-        for(i=2; i<=n; i++){
+        for (i = 2; i <= n; i++) {
             number = (long) (Math.random() * i); // random number of [0, i[
             // the leaf is in the box number+i -1
-            change_leaves((int)(i-1), (int)(number+i-1));
-            tree[(int)(i-1)].right_child = (int) (2*i-1);
-            tree[(int)(i-1)].left_child = (int) (2*i);
+            change_leaves((int) (i - 1), (int) (number + i - 1));
+            tree[(int) (i - 1)].right_child = (int) (2 * i - 1);
+            tree[(int) (i - 1)].left_child = (int) (2 * i);
 
-            tree[(int)(i-1)].num = (int) i;
-            tree[(int)(2*i-1)].parent = tree[(int)(2*i)].parent = (int)(i-1);
-            tree[(int)(2*i-1)].right_child = tree[(int)(2*i-1)].left_child = -1;
-            tree[(int)(2*i)].right_child = tree[(int)(2*i)].left_child = -1;
+            tree[(int) (i - 1)].num = (int) i;
+            tree[(int) (2 * i - 1)].parent = tree[(int) (2 * i)].parent = (int) (i - 1);
+            tree[(int) (2 * i - 1)].right_child = tree[(int) (2 * i - 1)].left_child = -1;
+            tree[(int) (2 * i)].right_child = tree[(int) (2 * i)].left_child = -1;
         }
     }
 
-    public Remy(long n, int[] list) {
+    public Remy(long n, ArrayList<Integer> list) {
         long i, number;
-        N=n;
-        tree = new Node[2*(int)n +1];
-        for(int ii=0; ii<tree.length; ii++)
-            tree[ii]=new Node();
+        N = n;
+        tree = new Node[2 * (int) n + 1];
+        for (int ii = 0; ii < tree.length; ii++)
+            tree[ii] = new Node();
 
         // built a tree of size 1
         tree[0].left_child = 1;
@@ -74,131 +82,121 @@ public class Remy {
         // the internal nodes will be in the boxes [0; i-1[ of the tree's array
         // the leaves in boxes [i, 2*i[ of the tree's array
 
-        for(i=2; i<=n; i++){
-            System.out.println("i="+i);
-            System.out.println("n="+n);
-            System.out.println("list[(int)i-2]*i = "+(list[(int)i-2] * i));
-            System.out.println("(i-1) = "+(i-1));
-            number = (long) (list[(int)i-2]); // random number of [0, i[
+        for (i = 2; i <= n; i++) {
+            System.out.println("i=" + i);
+            System.out.println("n=" + n);
+            System.out.println("list[(int)i-2]*i = " + (list.get((int) i - 2) * i));
+            System.out.println("(i-1) = " + (i - 1));
+            number = (long) (list.get((int) i - 2)); // random number of [0, i[
             // the leaf is in the box number+i -1
-            System.out.println("number = "+(number));
-            System.out.println("(number+i-1) = "+(number+i-1));
-            change_leaves((int)(i-1), (int)(number+i-1));
-            tree[(int)(i-1)].right_child = (int) (2*i-1);
-            tree[(int)(i-1)].left_child = (int) (2*i);
+            System.out.println("number = " + (number));
+            System.out.println("(number+i-1) = " + (number + i - 1));
+            change_leaves((int) (i - 1), (int) (number + i - 1));
+            tree[(int) (i - 1)].right_child = (int) (2 * i - 1);
+            tree[(int) (i - 1)].left_child = (int) (2 * i);
 
-            tree[(int)(i-1)].num = (int) i;
-            tree[(int)(2*i-1)].parent = tree[(int)(2*i)].parent = (int)(i-1);
-            tree[(int)(2*i-1)].right_child = tree[(int)(2*i-1)].left_child = -1;
-            tree[(int)(2*i)].right_child = tree[(int)(2*i)].left_child = -1;
+            tree[(int) (i - 1)].num = (int) i;
+            tree[(int) (2 * i - 1)].parent = tree[(int) (2 * i)].parent = (int) (i - 1);
+            tree[(int) (2 * i - 1)].right_child = tree[(int) (2 * i - 1)].left_child = -1;
+            tree[(int) (2 * i)].right_child = tree[(int) (2 * i)].left_child = -1;
         }
     }
 
     // à check
-    public boolean isLeaf(int i){
-       return tree[i].right_child==-1 && tree[i].left_child==-1;
+    public boolean isLeaf(int i) {
+        return tree[i].right_child == -1 && tree[i].left_child == -1;
     }
 
-    private String phi(int i){
-        if(isLeaf(i))
+    private String phi(int i) {
+        if (isLeaf(i))
             return "";
         else {
-            return "("+tree[i].left_child+")"+phi(tree[i].right_child);
+            return "(" + tree[i].left_child + ")" + phi(tree[i].right_child);
         }
     }
 
-    public String phi(){
+    public String phi() {
         return phi(0);
     }
 
-    private long fact(long n){
-        if (n==0){
+    private long fact(long n) {
+        if (n == 0) {
             return 1;
         }
 
-        return fact(n-1)*n;
+        return fact(n - 1) * n;
     }
 
-    /*private int[][] createList(){
-        int[][] lists = new int[(int)N][];
-
-
-        int list[] = new int[(int)N];
-        long nbLists = fact(N);
-        for(int i=0; i < fact(N); i++){
-            for()
-            lists[i] = ;
-        }
-
-        return ;
-    }*/
-
-    /*private static boolean coverageTestsForSizeN(long n){
-        Node[] differentTrees;
-        int[] counter;
-        Remy tree;
-
-        for(int i=0 ; i<n ; i++)
-            ;
-
-    }*/
-
-    public static int catalan(int n){
-        if(n==0 || n==1)
+    public static int catalan(int n) {
+        if (n == 0 || n == 1)
             return 1;
 
-        double res = (2. * (2. * ((double)n) - 3.) / ((double)n)) * catalan(n-1);
+        double res = (2. * (2. * ((double) n) - 3.) / ((double) n)) * catalan(n - 1);
 
-        return (int)res;
+        return (int) res;
     }
 
     // nombre de Catalan
-    public int catalan(){
-        return catalan((int)N);
+    public int catalan() {
+        return catalan((int) N);
     }
 
     // test de couverture
-    public static boolean coverageTests(int noeuds) { ;
+    public static boolean coverageTests(int noeuds) {
+        ;
         int diffTrees = catalan(noeuds);
         Remy[] differentTrees = new Remy[diffTrees];
         int[] counter = new int[diffTrees];
         boolean counted;
         int sizeOfDifferentTrees = 0;
 
-        int[] list = new int[noeuds];
-
-        for (int i = (int) noeuds - 1; i >= 0; i--){
-            for (int n = 0; n <= i; n++) {
-                list[i] = n;
-
-                System.out.println("list : "+list);
-                for(int a : list){
-                    System.out.print(a+" ");
+        //for (int i = (int) noeuds - 2; i >= 0; i--){
+        //   for (int n = 0; n < i; n++) {
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> liststmp = new ArrayList<>();
+        ArrayList<Integer> p = new ArrayList<>();
+        p.add(0);
+        lists.add(p);
+        for (int n = 1; n < noeuds - 1; n++) {
+            for (int m = 0; m <= n; m++) {
+                for (ArrayList v : lists) {
+                    ArrayList<Integer> l = new ArrayList();
+                    l.addAll(v);
+                    l.add(m);
+                    liststmp.add(l);
                 }
-                System.out.println();
+            }
+            lists = (ArrayList) liststmp.clone();
+        }
+        for (int i = 0; i < lists.size(); i++) {
 
-                Remy remy = new Remy(noeuds, list);
+            System.out.println("list : " + lists.get(i));
+            for (int a : lists.get(i)) {
+                System.out.print(a + " ");
+            }
+            System.out.println();
 
-                // parcours des arbres déjà présents
-                counted = false;
-                for(int k = 0; k < sizeOfDifferentTrees; k++)
-                    if(differentTrees[k].phi().equals(remy.phi())){
-                        counter[k]++;
-                        counted = true;
-                    }
+            Remy remy = new Remy(noeuds, lists.get(i));
 
-                if(!counted){
-                    differentTrees[sizeOfDifferentTrees]=remy;
-                    sizeOfDifferentTrees++;
+            // parcours des arbres déjà présents
+            counted = false;
+            for (int k = 0; k < sizeOfDifferentTrees; k++)
+                if (differentTrees[k].phi().equals(remy.phi())) {
+                    counter[k]++;
+                    counted = true;
                 }
+
+            if (!counted) {
+                differentTrees[sizeOfDifferentTrees] = remy;
+                sizeOfDifferentTrees++;
             }
         }
 
-        System.out.println("differentTrees.length "+ differentTrees.length);
+        System.out.println("differentTrees.length " + differentTrees.length);
 
         // uniformity?
-        for(int i=0; i<differentTrees.length-1; i++) {
-            System.out.println(i+": " + counter[i]);
+        for (int i = 0; i < differentTrees.length - 1; i++) {
+            System.out.println(i + ": " + counter[i]);
             if (counter[i] != counter[i + 1])
                 return false;
         }
@@ -206,7 +204,7 @@ public class Remy {
         return true;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Remy.coverageTests(6);
     }
 }
